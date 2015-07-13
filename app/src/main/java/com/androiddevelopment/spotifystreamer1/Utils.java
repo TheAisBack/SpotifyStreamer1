5d37f176c1c08d80dce94e5d3e657a195c189d94
Alan <alanhekle@hotmail.com>
2015-07-12 19:47:27 -0500

    Update Utils.java

--- app/src/main/java/com/androiddevelopment/spotifystreamer1/Utils.java
+++ app/src/main/java/com/androiddevelopment/spotifystreamer1/Utils.java
@@ -6,10 +6,8 @@ import kaaes.spotify.webapi.android.models.Image;
 public class Utils {
 
     public static final String TAG = Utils.class.getSimpleName();
-
     public static String getThumbnailUrl(List<Image> imagesList, int requiredSize) {
         String thumbnailUrl = null;
-
         Image image;
         int imageSize;
         //this code helps in improving the image of the thumbnail. large then small.
